a45249adad9765e1a9efb7fe5339a28fe2768064
gimzz <letig732@gmail.com>
2025-08-07 14:05:09 -0400

    feat(clientes): Se crea el moodelo de clientes
    Se crea el modelo de clientes con sus respectivos campos, constructores
    y encapsulamiento.

---
+++ src/main/java/com/mycompany/pay/orders/model/Clientes.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pay.orders.model;
+
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author gimz
+ */
+public class Clientes {
+    private int id;
+    private int cedula;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private LocalDateTime fechaRegistro;
+
+    public Clientes(int id, int cedula, String nombre, String apellido, String telefono, LocalDateTime fechaRegistro) {
+        this.id = id;
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.fechaRegistro = fechaRegistro;
+    }
+    
+    public Clientes(){}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public LocalDateTime getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(LocalDateTime fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+    
+    
+    
+
+    
+}
