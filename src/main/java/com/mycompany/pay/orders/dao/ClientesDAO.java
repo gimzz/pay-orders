25d67ff24fae91ff85f5e9fe9d9d3100ad2a1aad
gimzz <letig732@gmail.com>
2025-08-07 14:06:20 -0400

    feat(clientes): ClienteDAO
    Se declaran los metodos que se usaran en clientesDAOImpl

---
+++ src/main/java/com/mycompany/pay/orders/dao/ClientesDAO.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pay.orders.dao;
+import com.mycompany.pay.orders.model.Clientes;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ *
+ * @author gimz
+ */
+public interface ClientesDAO {
+    void agregarCliente(Clientes cliente) throws SQLException;
+    Clientes obtenerClientePorId(int id) throws SQLException;
+    List<Clientes> obtenerTodosLosClientes() throws SQLException;
+    void actualizarCliente(Clientes cliente) throws SQLException;
+    void eliminarCliente(int id) throws SQLException;
+    
+}
