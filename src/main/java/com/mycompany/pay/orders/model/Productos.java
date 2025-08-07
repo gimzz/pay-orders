df1c8adcc39493bbe75477ff540eaa5f66b5c1d6
gimzz <letig732@gmail.com>
2025-08-06 20:16:44 -0400

    feat: se crea el modelo, constructores de productos

---
+++ src/main/java/com/mycompany/pay/orders/model/Productos.java
@@ -0,0 +1,76 @@
+package com.mycompany.pay.orders.model;
+
+import java.math.BigDecimal;
+
+public class Productos {
+    private int id;
+    private String nombre;
+    private BigDecimal precioUsd;
+    private int stockActual;
+    private int stockMinimo;
+    private boolean activo;
+
+    
+    public Productos(int id, String nombre, BigDecimal precioUsd, int stockActual, int stockMinimo, boolean activo) {
+    this.id = id;
+    this.nombre = nombre;
+    this.precioUsd = precioUsd;
+    this.stockActual = stockActual;
+    this.stockMinimo = stockMinimo;
+    this.activo = activo;
+}
+
+    
+    
+    
+    public Productos() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public BigDecimal getPrecioUsd() {
+        return precioUsd;
+    }
+
+    public void setPrecioUsd(BigDecimal precioUsd) {
+        this.precioUsd = precioUsd;
+    }
+
+    public int getStockActual() {
+        return stockActual;
+    }
+
+    public void setStockActual(int stockActual) {
+        this.stockActual = stockActual;
+    }
+
+    public int getStockMinimo() {
+        return stockMinimo;
+    }
+
+    public void setStockMinimo(int stockMinimo) {
+        this.stockMinimo = stockMinimo;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+}
