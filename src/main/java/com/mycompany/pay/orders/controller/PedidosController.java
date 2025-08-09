96cbec834329d116e41c3b65facb4f0fb182a2ad
gimzz <letig732@gmail.com>
2025-08-08 20:38:21 -0400

    feat: Controlador de Pedidos
    Se crea el controlador de pedidos que maneja los DAOS de Pedido y
    Detalle_Pedido, ademas, de que utiliza la conexion ya que maneja varias
    transacciones, esto para que no haya errores al momento de alguna
    operacion

---
+++ src/main/java/com/mycompany/pay/orders/controller/PedidosController.java
@@ -0,0 +1,99 @@
+package com.mycompany.pay.orders.controller;
+
+import com.mycompany.pay.orders.dao.DetallePedidoDAOImpl;
+import com.mycompany.pay.orders.dao.PedidosDAOImpl;
+import com.mycompany.pay.orders.dao.ProductosDAO;
+import com.mycompany.pay.orders.model.DetallePedido;
+import com.mycompany.pay.orders.model.Pedidos;
+
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+public class PedidosController {
+
+    private PedidosDAOImpl pedidosDAO;
+    private DetallePedidoDAOImpl detallesDAO;
+    private ProductosDAO productosDAO;
+    private Connection connection;
+
+    public PedidosController(Connection connection, ProductosDAO productosDAO) {
+        this.connection = connection;
+        this.pedidosDAO = new PedidosDAOImpl(connection);
+        this.detallesDAO = new DetallePedidoDAOImpl(connection);
+        this.productosDAO = productosDAO;
+    }
+
+    public void crearPedidoConDetalles(Pedidos pedido, List<DetallePedido> detalles) throws SQLException {
+        try {
+            connection.setAutoCommit(false);
+
+            pedidosDAO.crearPedido(pedido);
+
+            for (DetallePedido detalle : detalles) {
+                detalle.setIdPedido(pedido.getId());
+                detallesDAO.agregarDetalle(detalle);
+            }
+
+            connection.commit();
+        } catch (SQLException ex) {
+            connection.rollback();
+            throw ex;
+        } finally {
+            connection.setAutoCommit(true);
+        }
+    }
+
+    public Pedidos obtenerPedido(int id) throws SQLException {
+        return pedidosDAO.obtenerPedidoPorId(id);
+    }
+
+    public List<DetallePedido> obtenerDetallesDePedido(int idPedido) throws SQLException {
+        return detallesDAO.obtenerDetallesPorPedido(idPedido);
+    }
+
+    public void actualizarEstadoEntrega(int pedidoId, boolean entregado) throws SQLException {
+        pedidosDAO.actualizarEstadoEntrega(entregado, pedidoId);
+    }
+
+    public void eliminarPedidoCompleto(int pedidoId) throws SQLException {
+        try {
+            connection.setAutoCommit(false);
+
+            detallesDAO.eliminarDetallesPorPedido(pedidoId);
+            pedidosDAO.eliminarPedido(pedidoId);
+
+            connection.commit();
+        } catch (SQLException ex) {
+            connection.rollback();
+            throw ex;
+        } finally {
+            connection.setAutoCommit(true);
+        }
+    }
+
+    public BigDecimal obtenerPrecioUnitarioProducto(int id) throws SQLException {
+        return productosDAO.obtenerPrecioUnitarioProducto(id);
+    }
+
+    public List<Pedidos> obtenerTodosLosPedidos() throws SQLException {
+        return pedidosDAO.obtenerTodosLosPedidos();
+    }
+
+    public List<Pedidos> obtenerPedidosEntregados() throws SQLException {
+        return pedidosDAO.obtenerPedidosEntregados();
+    }
+
+    public List<Pedidos> obtenerPedidosNoEntregados() throws SQLException {
+        return pedidosDAO.obtenerPedidosNoEntregados();
+    }
+
+    public void modificarDetalle(DetallePedido detalle) throws SQLException {
+        detallesDAO.actualizarDetalle(detalle);
+    }
+
+    public void eliminarDetalle(int idDetalle) throws SQLException {
+        detallesDAO.eliminarDetalle(idDetalle);
+    }
+}
