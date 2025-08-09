2c1171571126bdd3dff7fbf946edf7c23ee19890
gimzz <letig732@gmail.com>
2025-08-08 20:45:54 -0400

    feat: Se crea modelo de DetallePedido

---
+++ src/main/java/com/mycompany/pay/orders/model/DetallePedido.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pay.orders.model;
+
+import java.math.BigDecimal;
+
+/**
+ *
+ * @author gimz
+ */
+public class DetallePedido {
+
+    private int idPedido;
+    private int idProducto;
+    private int cantidad;
+    private BigDecimal precioUnitario;
+    private BigDecimal subtotalUsd;
+
+    public DetallePedido(){}
+    
+    public DetallePedido(int idPedido, int idProducto, int cantidad, BigDecimal precioUnitario, BigDecimal subtotalUsd) {
+        this.idPedido = idPedido;
+        this.idProducto = idProducto;
+        this.cantidad = cantidad;
+        this.precioUnitario = precioUnitario;
+        this.subtotalUsd = subtotalUsd;
+    }
+
+    public int getIdPedido() {
+        return idPedido;
+    }
+
+    public void setIdPedido(int idPedido) {
+        this.idPedido = idPedido;
+    }
+
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(int idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public BigDecimal getPrecioUnitario() {
+        return precioUnitario;
+    }
+
+    public void setPrecioUnitario(BigDecimal precioUnitario) {
+        this.precioUnitario = precioUnitario;
+    }
+
+    public BigDecimal getSubtotalUsd() {
+        return subtotalUsd;
+    }
+
+    public void setSubtotalUsd(BigDecimal subtotalUsd) {
+        this.subtotalUsd = subtotalUsd;
+    }
+
+}
